c728769683d119eceb7a0f6c5279dd60903f0b1e
celfiew <celfiew@gmail.com>
2021-12-27 22:28:20 -0500

    request api starwards

---
+++ http_call/request/geocode.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const argv = require('yargs').argv;
+
+let accion = argv.accion;
+let url = `https://swapi.dev/api/${accion}`;
+
+request({
+    url: url,
+    json: true
+},(error, response, body)=>{
+    if(error){
+        console.log('servicio no disponible');
+    }else if(body.detail === 'Not found'){
+        console.log('No hay datos a mostrar');
+    } else {
+        console.log(JSON.stringify(body, undefined, 2));
+        console.log(JSON.stringify(body.climate));
+        console.log(JSON.stringify(body.detail));
+        console.log(JSON.stringify(`nombre:  ${body.name}`));
+        console.log(JSON.stringify(`habitantes: ${body.population}`));
+        console.log(JSON.stringify(body.residents[0]));
+        console.log(JSON.stringify(body.films[2]));
+    }
+
+    });
